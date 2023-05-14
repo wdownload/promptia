96f10425c9152bc78e38b797c472b14b491b51d6
oldemiro baloi <oldemiroamerico@gmail.com>
2023-05-14 22:15:58 +0200

    fix api not updating

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -14,7 +14,7 @@ export const  GET = async (req) => {
        const prompts = await Prompt.find({}).populate("creator")
 
        
-       return new Response(JSON.stringify(),{status: 200})
+       return new Response(JSON.stringify(prompts),{status: 200})
  
      }
  
