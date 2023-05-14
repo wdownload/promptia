1b50888a97e00456783cd81659e53480bda5489d
oldemiro baloi <oldemiroamerico@gmail.com>
2023-05-14 22:48:20 +0200

    fix api not updating

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   reactStrictMode: true,
+   generateEtags: false,
     experimental: {
       appDir: true,
       serverComponentsExternalPackages: ["mongoose"],
