1c9232f72ed0da0c094f548d53a2688e6112d672
oldemiro baloi <oldemiroamerico@gmail.com>
2023-05-15 18:44:28 +0200

    add no caching for prompt request

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -35,7 +35,7 @@ const Feed = () => {
 
   useEffect(() =>{
     const fetchPosts = async () =>{
-      const response = await fetch("/api/prompt")
+      const response = await fetch("/api/prompt", { cache : "no-store"})
       const data = await response.json();
       setPosts(data);
     }
